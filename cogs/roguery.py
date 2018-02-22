a4207b0bf54554f573f5f4539f3ed7d4b35e55df
Jake P <101110@vivaldi.net>
2018-02-22 20:30:00 +0100

    Added the ded commadn

---
+++ cogs/roguery.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+
+class roguery:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def ded(self, ctx):
+        await self.bot.say("https://giphy.com/gifs/bare-barren-Az1CJ2MEjmsp2")
+
+
+def setup(bot):
+    bot.add_cog(roguery(bot))
