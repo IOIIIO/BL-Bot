0ef659369c20f2f2add494cc86ab0b0cb6c20555
IOIIIO <101110@vivaldi.net>
2018-08-07 20:12:17 +0200

    Fixed an error when eating nothing

--- cogs/eat.py
+++ cogs/eat.py
@@ -49,26 +49,27 @@ class Eat:
         """Randomly chooses a way to eat."""
         server = context.message.server
         author = context.message.author
-        if "<@!" in victim or "<@" in victim:
-            if "<@!" in victim:
-                user = victim.replace('<@!', '').replace('>', '')
-            if "<@" in victim:
-                user = victim.replace('<@', '').replace('>', '')
-            person = self.bot.get_user_info(user)
-            if user == author.id:
-                message = str(random.choice(self.eat_self))
-            elif user == self.bot.user.id:
-                message = str(random.choice(self.eat_bot))
-            elif user != author.id and user != self.bot.user.id:
-                message = str(random.choice(self.eat_person)).format(victim=victim)
-            await self.bot.say(italics(context.message.author.display_name)+ ', ' + message)
-        elif victim == None:
-            message = str(random.choice(self.eat_nothing))
-            await self.bot.say(italics(context.message.author.display_name)+ ', ' + message)
-        elif "@everyone" in victim or "@here" in victim:
-            await self.bot.say("Nice try funny guy")
+        if victim is not None:
+            if "<@!" in victim or "<@" in victim:
+                if "<@!" in victim:
+                    user = victim.replace('<@!', '').replace('>', '')
+                if "<@" in victim:
+                    user = victim.replace('<@', '').replace('>', '')
+                person = self.bot.get_user_info(user)
+                if user == author.id:
+                    message = str(random.choice(self.eat_self))
+                elif user == self.bot.user.id:
+                    message = str(random.choice(self.eat_bot))
+                elif user != author.id and user != self.bot.user.id:
+                    message = str(random.choice(self.eat_person)).format(victim=victim)
+                await self.bot.say(italics(context.message.author.display_name)+ ', ' + message)
+            elif "@everyone" in victim or "@here" in victim:
+                await self.bot.say("Nice try funny guy")
+            else:
+                message = str(random.choice(self.eat_thing)).format(victim=victim)
+                await self.bot.say(italics(context.message.author.display_name)+ ', ' + message)
         else:
-            message = str(random.choice(self.eat_thing)).format(victim=victim)
+            message = str(random.choice(self.eat_nothing))
             await self.bot.say(italics(context.message.author.display_name)+ ', ' + message)
 
         
